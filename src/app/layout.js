36dc3d029fe4da44f1a2c8556894ce4a4aba6930
arigree <arigree@iu.edu>
2024-11-09 20:06:42 -0500

    undo

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,13 @@
-
 import "./globals.css";
-
+import Nav from "@/components/Nav/Nav";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Nav />
+        {children}
+      </body>
     </html>
   );
 }
