f4c0188b7a01b1089648277dc28312cf6d8066a7
arigree <arigree@iu.edu>
2024-11-13 10:34:02 -0500

    setting up home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
 import LRButtons from "@/components/LRButtons";
 import styles from "./page.module.css";
 
+
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Home</h1>
+    <main className={styles.mainContent}>
+      <div className={styles.content}>
+        <h1>Home</h1>
+        <img className={styles.img} src="piranha.webp"></img>
       <LRButtons pageBack="" pageNext="inspo" />
+      </div>
+      
     </main>
   );
 }
