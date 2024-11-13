1f28a7308be3fea0749f9ed92cae975696cb719f
arigree <arigree@iu.edu>
2024-11-13 15:07:18 -0500

    home page

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,14 @@ export default function Home() {
   return (
     <main className={styles.mainContent}>
       <div className={styles.content}>
-        <h1>Home</h1>
+        
         <img className={styles.img} src="piranha.webp"></img>
       <LRButtons pageBack="" pageNext="inspo" />
+      <div className={styles.text}>
+        <h1 className={styles.h1}>Petey Piranha</h1>
+        <h3>He is the dude, like the bro, and he is cool, i think</h3>
       </div>
-      
+      </div>
     </main>
   );
 }
